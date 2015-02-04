151d3978071a0343be747a687bc0e14265870522
Wes <wesley.landriault@gmail.com>
2015-02-03 19:01:23 -0500

    Finished first two exercises.

---
+++ 01_temperature/temperature.rb
@@ -0,0 +1,7 @@
+def ftoc(fahr)
+	(fahr - 32) * 5/9
+end
+
+def ctof(cels)
+	cels * 9/5 + 32
+end
---
+++ 02_calculator/calculator.rb
@@ -0,0 +1,12 @@
+def add(num1, num2)
+	num1 + num2
+end
+
+def subtract(num1, num2)
+	num1 - num2
+end
+
+def sum(arr)
+	arr.inject{|sum, num| sum + num}
+end
+
